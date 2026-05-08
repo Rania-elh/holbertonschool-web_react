7a596444c4cf5c3c761d4c9807d9f7383645a7f1
Rania-elh <rania1elhefnawy@gmail.com>
2026-05-08 23:42:56 +0200

    projet

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -11,19 +11,34 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { esModule: false },
+          },
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'javascript/auto',
         use: [
           {
             loader: 'file-loader',
             options: {
               name: 'assets/[name].[hash].[ext]',
+              esModule: false,
             },
           },
           {
             loader: 'image-webpack-loader',
+            options: {
+              disable: false,
+              mozjpeg: { progressive: true, quality: 75 },
+              optipng: { enabled: true },
+              pngquant: { quality: [0.65, 0.9], speed: 4 },
+              gifsicle: { interlaced: false },
+            },
           },
         ],
       },
