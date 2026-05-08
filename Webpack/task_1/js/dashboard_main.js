dcd005ca969847b29d301f78a0d9ad3c8509a331
Rania-elh <rania1elhefnawy@gmail.com>
2026-05-08 23:49:43 +0200

    projet

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,12 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
 let count = 0;
 
 function updateCounter() {
@@ -8,16 +14,4 @@ function updateCounter() {
   $('#count').text(`${count} clicks on the button`);
 }
 
-$(() => {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-
-  const $button = $('<button>Click here to get started</button>');
-  $('body').append($button);
-
-  $('body').append("<p id='count'></p>");
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $button.on('click', _.debounce(updateCounter, 500));
-});
-
+$('button').on('click', _.debounce(updateCounter, 500));
--- Webpack/task_1/webpack.config.js
+++ Webpack/task_1/webpack.config.js
@@ -8,4 +8,3 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
   },
 };
-
